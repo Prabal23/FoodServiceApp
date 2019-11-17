f4f7d832d67829d0c8ffecf9768eb3fd08aa90a5
prabal23 <prabal230193@gmail.com>
2019-11-17 16:02:05 +0600

    Food service App

---
+++ lib/Cards/PortraitFoodCard/PortraitFoodCard.dart
@@ -0,0 +1,260 @@
+import 'package:flutter/material.dart';
+import 'package:foodpanda_app/MainScreen/FoodDetailsPage/details.dart';
+
+import '../../main.dart';
+
+class PortraitFoodCard extends StatefulWidget {
+  final index;
+  PortraitFoodCard(this.index);
+  @override
+  _PortraitFoodCardState createState() => _PortraitFoodCardState();
+}
+
+class _PortraitFoodCardState extends State<PortraitFoodCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DetailsPage()),
+          );
+        },
+        child: ListTile(
+          title: Stack(
+            children: <Widget>[
+              Container(
+                //width: 100,
+                padding: EdgeInsets.only(bottom: 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Center(
+                      child: Container(
+                        //color: Colors.red,
+
+                        padding: const EdgeInsets.all(0.0),
+                        margin: EdgeInsets.only(top: 0),
+                        child: Image.asset(
+                          widget.index % 2 == 0
+                              ? 'assets/burger.jpg'
+                              : 'assets/roll.jpg',
+                          height: 100,
+                          width: 120,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      child: VerticalDivider(
+                        //indent: 2.0,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                widget.index % 2 == 0
+                                    ? "Restaurant Name"
+                                    : "Restaurant Name from list",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  //Icon(Icons.label_important, size: 15, color: header,),
+                                  Container(
+                                    margin: EdgeInsets.only(left: 0),
+                                    child: Text(
+                                        widget.index % 2 == 0
+                                            ? "Thai, Chinese"
+                                            : "Italian, Chinese, Thai",
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            fontSize: 13,
+                                            color: Colors.black87)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 8, left: 0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  // Container(
+                                  //   child: Row(
+                                  //     children: <Widget>[
+                                  //       Icon(
+                                  //         Icons.star,
+                                  //         color: golden,
+                                  //         size: 12,
+                                  //       ),
+                                  //       Icon(
+                                  //         Icons.star,
+                                  //         color: golden,
+                                  //         size: 12,
+                                  //       ),
+                                  //       Icon(
+                                  //         Icons.star,
+                                  //         color: golden,
+                                  //         size: 12,
+                                  //       ),
+                                  //       Icon(
+                                  //         Icons.star,
+                                  //         color: golden,
+                                  //         size: 12,
+                                  //       ),
+                                  //       Icon(
+                                  //         Icons.star,
+                                  //         color: golden,
+                                  //         size: 12,
+                                  //       ),
+                                  //     ],
+                                  //   ),
+                                  // ),
+                                  Expanded(
+                                    child: Container(
+                                      margin: EdgeInsets.only(
+                                          right: 8, top: 0, bottom: 10),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.attach_money,
+                                            size: 13,
+                                            color: header,
+                                          ),
+                                          Expanded(
+                                            child: Container(
+                                              margin: EdgeInsets.only(left: 3),
+                                              child: Text(
+                                                  widget.index % 2 == 0
+                                                      ? "Free Delivery"
+                                                      : "50 (Delivery Charge)",
+                                                  maxLines: 1,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.grey,
+                                                      fontWeight:
+                                                          FontWeight.bold)),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Container(
+                                    //color: Colors.red,
+                                    margin: EdgeInsets.only(
+                                        right: 8, top: 0, bottom: 0, left: 0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.timer,
+                                          size: 13,
+                                          color: header,
+                                        ),
+                                        Container(
+                                          margin: EdgeInsets.only(left: 4),
+                                          child: Text(
+                                              widget.index % 2 == 0
+                                                  ? "35 MIN"
+                                                  : "1 HR",
+                                              style: TextStyle(
+                                                  color: Colors.grey[500],
+                                                  fontSize: 11)),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    child: Row(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.star,
+                                          color: header,
+                                          size: 12,
+                                        ),
+                                        Container(
+                                            margin: EdgeInsets.only(left: 3),
+                                            child: Text("5.0",
+                                                style: TextStyle(
+                                                    color: Colors.black54,
+                                                    fontSize: 12))),
+                                        Container(
+                                            margin: EdgeInsets.only(left: 2),
+                                            child: Text("(30)",
+                                                style: TextStyle(
+                                                    color: Colors.black54,
+                                                    fontSize: 12)))
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.all(5),
+                        color: widget.index % 2 == 0
+                            ? header.withOpacity(0.7)
+                            : Colors.transparent,
+                        child: Text(
+                          widget.index % 2 == 0 ? "20% OFF" : "",
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        )),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Forms/ProfileEditForm/profileEditForm.dart
@@ -0,0 +1,253 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+
+import '../../main.dart';
+
+class ProfileEditForm extends StatefulWidget {
+  @override
+  _ProfileEditFormState createState() => _ProfileEditFormState();
+}
+
+class _ProfileEditFormState extends State<ProfileEditForm> {
+  int _current = 0;
+  int _isBack = 0;
+  String result = '', date = 'Select Birth Date';
+  String _radioGender;
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController addressController = new TextEditingController();
+  TextEditingController phoneController = new TextEditingController();
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController bdController = new TextEditingController();
+  var dd, finalDate;
+  DateTime _date = DateTime.now();
+  int gen = 1, gen1 = 0;
+  Future<File> fileImage;
+
+  @override
+  void initState() {
+    nameController.text = 'John Smith';
+    addressController.text = 'Modina Market';
+    phoneController.text = '017XXXXXXXX';
+    emailController.text = 'john.smith@gmail.com';
+    bdController.text = '23/01/1993';
+    var now = new DateTime.now();
+    date = new DateFormat("dd/MM/yyyy").format(now);
+    super.initState();
+  }
+
+  pickImagefromGallery(ImageSource src) {
+    setState(() {
+      fileImage = ImagePicker.pickImage(source: src);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                FutureBuilder<File>(
+                  future: fileImage,
+                  builder:
+                      (BuildContext context, AsyncSnapshot<File> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done &&
+                        snapshot.data != null) {
+                      return Container(
+                        //transform: Matrix4.translationValues(0.0, 0.0, 0.0),
+                        padding: EdgeInsets.all(1.0),
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: FileImage(snapshot.data),
+                        ),
+                        decoration: new BoxDecoration(
+                          color: Colors.grey, // border color
+                          shape: BoxShape.circle,
+                        ),
+                      );
+                    } else if (snapshot.error != null) {
+                      return const Text(
+                        'Error Picking Image',
+                        textAlign: TextAlign.center,
+                      );
+                    } else {
+                      return Container(
+                        //transform: Matrix4.translationValues(0.0, 0.0, 0.0),
+                        padding: EdgeInsets.all(1.0),
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: AssetImage('assets/user.png'),
+                        ),
+                        decoration: new BoxDecoration(
+                          color: Colors.grey, // border color
+                          shape: BoxShape.circle,
+                        ),
+                      );
+                    }
+                  },
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    pickImagefromGallery(ImageSource.gallery);
+                  },
+                  child: Container(
+                    width: 160,
+                    margin: EdgeInsets.only(left: 0, right: 0, top: 10),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        color: Colors.grey[200],
+                        border: Border.all(width: 0.2, color: Colors.grey)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.image,
+                          size: 16,
+                          color: Colors.black54,
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(left: 5),
+                            child: Text("Select photo",
+                                style: TextStyle(
+                                    color: Colors.black54, fontSize: 13)))
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 20), height: 2, child: Divider()),
+          Container(
+              padding:
+                  EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 10),
+              child: Container(
+                  margin: EdgeInsets.only(left: 8, top: 0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      color: Colors.grey[100],
+                      border: Border.all(width: 0.2, color: Colors.grey)),
+                  child: TextField(
+                    controller: nameController,
+                    autofocus: true,
+                    style: TextStyle(color: Colors.black54),
+                    decoration: InputDecoration(
+                      hintText: 'Type your name...',
+                      labelText: 'Enter Name',
+                      labelStyle: TextStyle(color: header),
+                      contentPadding: EdgeInsets.fromLTRB(10.0, 5.0, 20.0, 5.0),
+                      border: InputBorder.none,
+                    ),
+                  ))),
+          // Container(
+          //     margin: EdgeInsets.only(top: 0),
+          //     height: 2,
+          //     child: Divider()),
+          Container(
+              padding:
+                  EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 10),
+              child: Container(
+                  margin: EdgeInsets.only(left: 8, top: 0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      color: Colors.grey[100],
+                      border: Border.all(width: 0.2, color: Colors.grey)),
+                  child: TextField(
+                    controller: addressController,
+                    style: TextStyle(color: Colors.black54),
+                    decoration: InputDecoration(
+                      hintText: 'Type your address...',
+                      labelText: 'Enter Address',
+                      labelStyle: TextStyle(color: header),
+                      contentPadding: EdgeInsets.fromLTRB(10.0, 5.0, 20.0, 5.0),
+                      border: InputBorder.none,
+                    ),
+                  ))),
+          // Container(
+          //     margin: EdgeInsets.only(top: 0),
+          //     height: 2,
+          //     child: Divider()),
+          Container(
+              padding:
+                  EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 10),
+              child: Container(
+                  margin: EdgeInsets.only(left: 8, top: 0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      color: Colors.grey[100],
+                      border: Border.all(width: 0.2, color: Colors.grey)),
+                  child: TextField(
+                    controller: phoneController,
+                    style: TextStyle(color: Colors.black54),
+                    decoration: InputDecoration(
+                      hintText: 'Type your phone number...',
+                      labelText: 'Enter Phone Number',
+                      labelStyle: TextStyle(color: header),
+                      contentPadding: EdgeInsets.fromLTRB(10.0, 5.0, 20.0, 5.0),
+                      border: InputBorder.none,
+                    ),
+                  ))),
+          Container(
+              margin: EdgeInsets.only(top: 0), height: 2, child: Divider()),
+          Container(
+              padding:
+                  EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 10),
+              child: Container(
+                  margin: EdgeInsets.only(left: 8, top: 0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      color: Colors.grey[100],
+                      border: Border.all(width: 0.2, color: Colors.grey)),
+                  child: TextField(
+                    controller: emailController,
+                    style: TextStyle(color: Colors.black54),
+                    decoration: InputDecoration(
+                      hintText: 'Type your email...',
+                      labelText: 'Enter Email',
+                      labelStyle: TextStyle(color: header),
+                      contentPadding: EdgeInsets.fromLTRB(10.0, 5.0, 20.0, 5.0),
+                      border: InputBorder.none,
+                    ),
+                  ))),
+          Container(
+            margin: EdgeInsets.only(left: 25, right: 15, bottom: 20, top: 5),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                color: header,
+                border: Border.all(width: 0.2, color: Colors.grey)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.edit,
+                  size: 20,
+                  color: Colors.white,
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 5),
+                    child: Text("Edit",
+                        style: TextStyle(color: Colors.white, fontSize: 17)))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
